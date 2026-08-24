abedc3fd1ebde4effd27059ef9ecdd7834f0326a
qurbonovsiyovush-dotcom <qurbonovsiyovush1009@gmail.com>
2026-08-24 10:20:57 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: [
+    "@napi-rs/canvas",
+  ],
 };
 
 export default nextConfig;
